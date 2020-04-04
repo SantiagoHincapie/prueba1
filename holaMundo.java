08a1524436045a5f33c41a8671db612ab7c41a6d
santiago <santiago.augusto48@gmail.com>
2020-04-03 22:27:36 -0500

    tercer commit

--- holaMundo.java
+++ holaMundo.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class holaMundo
 {
     public static void main(String[] args)
@@ -9,6 +12,19 @@ public class holaMundo
         //boolean siempre seran del tipo boolean desicion=TRUE o false
         //datos de tipos no primitibos son aquellos que almacenan NULL ademas tambien tienen metodos estos datos son :Integer,string
         //las constantes se declaran con la palabra final antes de la declaracion del tipo de dato ej:final int=10;
+        //JOptionPane Es la entrada y salida de datos pero con ventanas emergentes
+
+        Scanner lector=new Scanner(System.in);
+        String cadena;
+        int numero;
+        char letra;
+        double decimal;
+
+        cadena= JOptionPane.showInputDialog("Digite su nombre:");
+        numero=Integer.parseInt(JOptionPane.showInputDialog("Digite el numeo de su cuenta"));
+        letra=JOptionPane.showInputDialog("Dijite una letra").charAt(0);
+        decimal=Double.parseDouble(JOptionPane.showInputDialog("Digite un decimal"));
+        
 
     }
 }
