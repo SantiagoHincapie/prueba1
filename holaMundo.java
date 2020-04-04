519c21509d3bb17f576f79991bbe93b9ee5b51a6
santiago <santiago.augusto48@gmail.com>
2020-04-03 21:55:33 -0500

    segundo commit

--- holaMundo.java
+++ holaMundo.java
@@ -2,6 +2,13 @@ public class holaMundo
 {
     public static void main(String[] args)
     {
-     System.out.println("Hola mundo");
+     //int ocupa 32 bit(va desde -2millones147mil hasta 2millones147mil) de memoria y long que ocupa 64bit de memoria
+        //para los float colocamos el numerodecimalf y la f al final mientras que con el double no.
+        //EL concatenador es el simbolo +
+        //el dato de tipo char es de tipo ''
+        //boolean siempre seran del tipo boolean desicion=TRUE o false
+        //datos de tipos no primitibos son aquellos que almacenan NULL ademas tambien tienen metodos estos datos son :Integer,string
+        //las constantes se declaran con la palabra final antes de la declaracion del tipo de dato ej:final int=10;
+
     }
 }
